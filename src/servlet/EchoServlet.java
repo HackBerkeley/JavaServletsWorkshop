5d23ed183b2c7af01d7cd369d663d93467b2ba4b
Gerald <geraldgfong@gmail.com>
2011-11-16 17:50:10 -0800

    Added Remember Servlet

--- src/servlet/EchoServlet.java
+++ src/servlet/EchoServlet.java
@@ -10,30 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class EchoServlet extends HttpServlet {
-
-	private List<String> names;
-	
-	@Override
-	public void init() {
-		
-		 names = new ArrayList<String>();
-	}
-
 	@Override
 	public void doGet( HttpServletRequest request, HttpServletResponse response) {
+		String name = request.getParameter("name");
+		PrintWriter out;
 		try {
-			
-			String name = request.getParameter("name");
-			PrintWriter out = response.getWriter();
+			out = response.getWriter();
 			out.println("Hello " + name + " dude check out all these people who have visited!");
-			
-			for( String otherName : names) {
-				out.println(otherName);
-			}
-			out.close();
-			names.add(name);
-			
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
